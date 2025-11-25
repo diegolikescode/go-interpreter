7358884fd59e492c0398973b3e4b0bc88814d756
diegolikescode <diegoprestes.git@hotmail.com>
2025-11-25 12:28:02 -0300

    start my interpreter

---
+++ internal/token/token.go
@@ -0,0 +1,39 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifiers and literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
