c1568cb50fa12420019777e3d6ddec54014c852a
diegolikescode <diegoprestes.git@hotmail.com>
2025-11-26 11:52:59 -0300

    small REPL for testing

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/diegolikescode/go-interpreter/internal/repl"
+)
 
 func main() {
-	fmt.Println("hey hey people")
+	fmt.Println("Hey hey programmer, this is your REPL")
+
+	repl.Start(os.Stdin, os.Stdout)
 }
